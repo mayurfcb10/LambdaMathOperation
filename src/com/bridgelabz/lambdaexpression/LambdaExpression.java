85e95113dedf1ffbf1f20a6372f53958a2380fc9
Mayur <mayurzope555@gmail.com>
2020-10-04 20:51:31 +0530

    [Mayur]Added: Lambda expression to perform math operation

--- src/com/bridgelabz/lambdaexpression/LambdaExpression.java
+++ src/com/bridgelabz/lambdaexpression/LambdaExpression.java
@@ -1,9 +1,30 @@
 package com.bridgelabz.lambdaexpression;
 
 public class LambdaExpression {
+	@FunctionalInterface
+	interface IMathFunction {
+		int calculate(int a, int b);
+	}
+	public static int add(int a, int b) {
+		return Math.addExact(a, b);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Lambda Math Operation Problem ");
-	}
+		
+		IMathFunction add1 = Integer::sum; // directly call method reference
+		IMathFunction  addition= (x, y) -> x + y;
+		IMathFunction  subtraction= (x, y) -> x - y;
+		IMathFunction  multiply= (x, y) -> x * y;
+		IMathFunction  divide= (x, y) -> x / y;
 
+
+		System.out.println("Addition is "+ addition.calculate(6, 3));
+		System.out.println("Multiplication is "+ multiply.calculate(6, 3));
+		System.out.println("Division is "+ divide.calculate(6, 3));
+		System.out.println("Subtration  is "+ subtraction.calculate(6, 3));
+
+
+	}
 }
+
