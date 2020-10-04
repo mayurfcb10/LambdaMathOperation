46d885efb4c3d4e2518ff1a6904131039d545944
Mayur <mayurzope555@gmail.com>
2020-10-04 20:47:22 +0530

    [Mayur]Added: Welcome to the lambda Math Operations

---
+++ src/com/bridgelabz/lambdaexpression/LambdaExpression.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.lambdaexpression;
+
+public class LambdaExpression {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Lambda Math Operation Problem ");
+	}
+
+}
